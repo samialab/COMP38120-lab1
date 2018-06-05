da0d4c283a1cfe0e1cd9271e0bf13f5d24a326ee
samialab <6316945+samialab@users.noreply.github.com>
2018-06-05 01:32:36 +0100

    Add admin username default

--- submit-job.sh
+++ submit-job.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Configure deployment point and optional parameters
 [ -z "${LOGIN}" ] && { echo "Environment variable LOGIN is not set, defaulting user to sshuser"; LOGIN="sshuser"; }
+[ -z "${ADMIN_LOGIN}" ] && { echo "Environment variable ADMIN_LOGIN is not set, defaulting admin-username to admin"; ADMIN_LOGIN="admin"; }
 [ -z "${CLUSTER_NAME}" ] && { echo "Environment variable CLUSTER_NAME is not set."; read -p "Enter the Azure HDInsight cluster name: " CLUSTER_NAME; }
 read -p "Enter the password to your admin account: " SEC
 read -p "Enter number of reducers to run: " NUM_REDUCER
@@ -12,7 +13,7 @@ JAR_LOCATION="./target/${JAR_NAME}"
 
 # Validate login
 echo "Authenticating your account"
-STATE=`curl -u admin:${SEC} -G https://${CLUSTER_NAME}.azurehdinsight.net/templeton/v1/status | jq .status `
+STATE=`curl -u ${ADMIN_LOGIN}:${SEC} -G https://${CLUSTER_NAME}.azurehdinsight.net/templeton/v1/status | jq .status `
 if [ -z "${STATE}" -o "${STATE}" = "" -o "${STATE}" != '"ok"' ] ; then
     echo "Authentication failed, exiting"
     exit 1
@@ -27,7 +28,7 @@ scp ${JAR_LOCATION} ${LOGIN}@${CLUSTER_NAME}-ssh.azurehdinsight.net:${JAR_NAME}
 ssh ${LOGIN}@${CLUSTER_NAME}-ssh.azurehdinsight.net hadoop fs -put -f ${JAR_NAME}
 
 echo "Executing the mapreduce job"
-JOB_ID=`curl -u admin:${SEC} -d user.name=${LOGIN} -d jar=${JAR_NAME} -d class=${CLASS_NAME} -d arg="-input" -d arg="/input/" -d arg="-output" -d arg="/comp38120/output" -d arg="-numReducers" -d arg=${NUM_REDUCER} https://${CLUSTER_NAME}.azurehdinsight.net/templeton/v1/mapreduce/jar | jq .id`
+JOB_ID=`curl -u ${ADMIN_LOGIN}:${SEC} -d user.name=${LOGIN} -d jar=${JAR_NAME} -d class=${CLASS_NAME} -d arg="-input" -d arg="/input/" -d arg="-output" -d arg="/comp38120/output" -d arg="-numReducers" -d arg=${NUM_REDUCER} https://${CLUSTER_NAME}.azurehdinsight.net/templeton/v1/mapreduce/jar | jq .id`
 
 echo "Job id: ${JOB_ID}"
 
